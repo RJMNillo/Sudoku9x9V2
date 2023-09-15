50c09c5dbde87c8f7233a8fa52d9f4e68b8f6f43
RJMNillo <rnillo011200@gmail.com>
2023-09-14 21:40:54 -0400

    Edits Moment

--- Sudoku9x9V2Solver.py
+++ Sudoku9x9V2Solver.py
@@ -1,20 +1,27 @@
 from Sudoku9x9 import Sudoku9x9
 
-def Sudoku9x9Solver():
+class Sudoku9x9Solver():
     def __init__(self, square = None):
-        self.ssquare = Sudoku9x9(square)
+        self.square = Sudoku9x9(square)
         # Basically just that, really
 
     # Helper Functions
     # checkEmpty: check if there are empty squares
     def checkEmpty(self):
-
-        ssquare.printSquare()
-
+        print("The current state of our square is: ")
+        self.square.printSquare()
         isEmpty = False
-        for row in range(0, len(self.ssquare)):
-            if 0 in self.ssquare[row]:
+        for row in range(0, len(self.square.square)):
+            if 0 in self.square.square[row]:
                 isEmpty = True
+        if isEmpty:
+            print("There are still empty spots in the table.")
+        else:
+            print("You have filled the table. GG")
+        return isEmpty
+    
     # checkBlocked: check if there are any squares blocked
+    def checkBlocked(self):
+        pass
 
     
